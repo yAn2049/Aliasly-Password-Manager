1092131533ef0de130de13aaa194ceb6eee60085
yAn <rapcsakjanos2@gmail.com>
2025-03-16 17:43:53 +0100

    Adatbázis és C# kezdete
    C# osztályok létrehozása
    C# és Sql összekötése
    C#-on belül adat lekérés az adatbázisból

---
+++ Aliasly/Aliasly/MVVM/Model/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliasly.MVVM.Model
+{
+    public class Log
+    {
+        public int LogId { get; set; }
+        public DateTime DatumIdo { get; set; }
+        public string IPcim { get; set; }
+        public string Leiras { get; set; }
+    }
+}
---
+++ Aliasly/Aliasly/MVVM/ViewModel/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Aliasly.MVVM.Model;
+using MySql.Data.MySqlClient;
+
+namespace Aliasly;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    List<Adatok> adatok = new List<Adatok>();
+    string connParams = "server=localhost;user=root;database=aliasly;port=3306";
+    public MainWindow()
+    {
+        InitializeComponent();
+        StartUp();
+
+        MySqlConnection connection = new MySqlConnection(connParams);
+        try
+        {
+            connection.Open();
+
+            string sqlAdatok = "SELECT id, jelszo, email, nev, url, hozzafuzes, mester_id FROM adatok";
+            MySqlCommand sqlCommand = new MySqlCommand(sqlAdatok, connection);
+            MySqlDataReader sqlReader = sqlCommand.ExecuteReader();
+
+            while (sqlReader.Read())
+            {
+                Adatok a = new Adatok()
+                {
+                    Id = int.Parse(sqlReader["id"].ToString()),
+                    Jelszo = sqlReader["jelszo"].ToString(),
+                    EMail = sqlReader["email"].ToString(),
+                    Nev = sqlReader["nev"].ToString(),
+                    Url = sqlReader["url"].ToString(),
+                    Hozzafuzes = sqlReader["hozzafuzes"].ToString(),
+                    MesterId = int.Parse(sqlReader["mester_id"].ToString())
+                };
+                adatok.Add(a);
+            }
+            sqlReader.Close();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Adatbázis csatlakozás error!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        foreach (var a in adatok)
+        {
+            this.lista.ItemsSource = adatok;
+        }
+
+    }
+    public void StartUp()
+    {
+
+    }
+}
