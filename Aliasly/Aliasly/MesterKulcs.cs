59b0d9983b408b0cac3c96e92fa5cd6f2103c408
yAn <rapcsakjanos2@gmail.com>
2025-03-17 21:03:27 +0100

    C# fejlődések
    Adatbázis kezelés tovább fejleszve, félis megvan az adatbevitel is

---
+++ Aliasly/Aliasly/MesterKulcs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliasly
+{
+    public class MesterKulcs
+    {
+        public int MesterId { get; set; }
+        public string KulcsString { get; set; }
+        public string SaltString { get; set; }
+        public string HashedKulcs { get; set; }
+    }
+}
