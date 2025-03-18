f04f1d18cc86685222ce931a73e0e4f7579dee5f
yAn <rapcsakjanos2@gmail.com>
2025-03-18 21:26:22 +0100

    Kisebb változások

--- Aliasly/Aliasly/obj/Debug/net8.0-windows/Aliasly.AssemblyInfo.cs
+++ Aliasly/Aliasly/obj/Debug/net8.0-windows/Aliasly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aliasly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0a1e446f32c44b08127a19f42d2ee0dd4a6cd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b70789084d62cace19d77a96048ff47f4c7faea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aliasly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aliasly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
